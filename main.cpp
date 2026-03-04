d8da5aeb614819ba109c7f15be9881ec89a0ca57
FShaaM <zinovevas135@gmail.com>
2026-03-04 14:04:55 +0300

    Implement add (insert before) and insert (insert after)

--- main.cpp
+++ main.cpp
@@ -10,5 +10,41 @@ struct BiList {
 	{}
 };
 
+template< class T > // добавить перед
+BiList<T>* add(BiList<T>* h, const T& d)
+{
+	BiList<T>* newNode = new BiList<T>;
+	newNode->val = d; // Оператор копирующего присваивания
+
+	if (h)
+	{
+		newNode->next = h;
+		h->prev = newNode;
+	}
+
+	return newNode;
+}
+
+template< class T > // добавить после
+BiList<T>* insert(BiList<T>* h, const T& d)
+{
+	BiList<T>* newNode = new BiList<T>; // Конструктор по умолчанию обнулил next и prev
+	newNode->val = d; // Оператор копирующего присваивания
+
+	if (h)
+	{
+		newNode->next = h->next;
+		newNode->prev = h;
+
+		if (h->next != nullptr)
+			h->next->prev = newNode;
+
+		h->next = newNode;
+	}
+
+	return newNode;
+}
+
+
 int main()
 {}
